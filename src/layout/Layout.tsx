2ba7877a183c5223d87f5aab5bf3809c16c2791c
SahilMaxd <sahil.thakkar2@gmail.com>
2025-01-29 18:28:17 -0600

    Updated recovery login to use otp code instead of magic link

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,34 +1,22 @@
 import './Layout.css';
-import { useEffect } from 'react';
 import { Redirect, Route, Switch } from 'wouter';
 import { LoginPage, RecoverPage, DashboardPage, SSGPage, ProfilePage } from '@pages/';
 import { Nav } from '@components/';
-import { useAppState, useSupabase } from '@hooks/';
+import { useAppState } from '@hooks/';
 import { PropsBase } from '@_types/';
 
 interface Props extends PropsBase {
-  path: string
+  path: string,
+  redirectPath?: string
 }
 
 export const Layout = () => {
-  const client = useSupabase();
-  const { isLoggedIn, setLoggedInState } = useAppState();
+  const { isLoggedIn } = useAppState();
 
-  useEffect(() => {
-    const { data } = client.auth.onAuthStateChange((event) => {
-      if (event === 'PASSWORD_RECOVERY' && !isLoggedIn) {
-        // If user enters site from password recovery email, reset app state
-        setLoggedInState();
-      }
-    });
-
-    return () => data.subscription.unsubscribe();
-  }, [client, isLoggedIn, setLoggedInState]);
-
-  const AuthPath = ({ children, path }: Props) => {
+  const AuthRoute = ({ children, path, redirectPath }: Props) => {
     return (
       <Route path={path}>
-        { isLoggedIn ? <Redirect to='/' /> : <>{children}</>}
+        { isLoggedIn ? <Redirect to={redirectPath ?? '/'} /> : <>{children}</>}
       </Route>
     )
   };
@@ -46,8 +34,8 @@ export const Layout = () => {
       <Nav />
       <div className='main'>
         <Switch>
-          <AuthPath path='/login'><LoginPage /></AuthPath>
-          <AuthPath path='/recover'><RecoverPage /></AuthPath>
+          <AuthRoute path='/login'><LoginPage /></AuthRoute>
+          <AuthRoute path='/recover' redirectPath='/profile'><RecoverPage /></AuthRoute>
           
           <ProtectedRoute path='/'><DashboardPage /></ProtectedRoute>
           <ProtectedRoute path='/ssg'><SSGPage /></ProtectedRoute>
